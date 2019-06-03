8a5a874fc6c073a6408b2719f5c5a381b4c3c8e1
Rose <maja.m.elliott@gmailcom>
2019-06-03 14:59:25 -0700

    Minor fixes
    Fixing walking in all scenes

--- New Game Project/scenes/Player.gd
+++ New Game Project/scenes/Player.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
-# Declare member variables here. Examples:
-const SPEED = 70
+# Movement speed
+const SPEED = 100
 var movedir = Vector2(0,0)
 
 # Called when the node enters the scene tree for the first time.
@@ -16,8 +16,8 @@ func _process(delta):
 func controls_loop():
 	var LEFT  = Input.is_action_pressed("ui_left")
 	var RIGHT = Input.is_action_pressed("ui_right")
-	var UP    = Input.is_action_pressed("ui_up")
-	var DOWN  = Input.is_action_pressed("ui_down")
+	var UP    = Input.is_action_pressed("ui_down")
+	var DOWN  = Input.is_action_pressed("ui_up")
 	
 	movedir.x = -int(LEFT) + int(RIGHT)
 	movedir.y = -int(DOWN) + int(UP)
--- New Game Project/scenes/RichTextLabel.gd
+++ New Game Project/scenes/RichTextLabel.gd
@@ -11,15 +11,15 @@ func _ready():
 	set_process_input(true)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _input(event):
-	if event.type == InputEvent.MOUSE_BUTTON && event.is_action_pressed():
-		if get_visible_characters() > get_total_character_count():
-				if page < dialogue.size()-1:
-					page += 1
-					set_bbcode(dialogue[page])
-					set_visible_characters(0)
-		else:
-			set_visible_characters(get_total_character_count())
+# func _input(event):
+	# if event.type == InputEvent.MOUSE_BUTTON && event.is_action_pressed():
+		# if get_visible_characters() > get_total_character_count():
+				# if page < dialogue.size()-1:
+					# page += 1
+					# set_bbcode(dialogue[page])
+					# set_visible_characters(0)
+		# else:
+			# set_visible_characters(get_total_character_count())
 
 func _on_Timer_timeout():
 	set_visible_characters(get_visible_characters()+1)
