39cece3e80976d5966465d084301ef8ba53fb016
Sebastian <scarabbeetlearmy@gmail.com>
2019-06-09 22:15:16 -0700

    changes by Maja (still need to fix base)

---
+++ New Game Project/Ghost.gd
@@ -0,0 +1,20 @@
+extends base.class
+
+const SPEED = 400
+
+var movetimer_length = 15
+var movetimer = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	movedir = dir.rand()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	movement_loop()
+	if movetimer > 0:
+		movetimer -= 1
+	if movetimer == 0 || is_on_wall():
+		movedir = dir.rand()
+		movetimer = movetimer_length
+		
